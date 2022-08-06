3fa5374a46ce033d738cad78fd154643af8a39a1
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2022-08-06 14:14:36 +0200

    added ft_replace_char, fixed ft_isnotcharset, updated Makefile and ft_conversions.h

--- srcs/conversions/ft_replace_char.c
+++ srcs/conversions/ft_replace_char.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isnotcharset.c                                  :+:      :+:    :+:   */
+/*   ft_replace_char.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/10 08:48:34 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/08/06 14:13:46 by ljohnson         ###   ########lyon.fr   */
+/*   Created: 2022/08/06 13:56:37 by ljohnson          #+#    #+#             */
+/*   Updated: 2022/08/06 14:03:06 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int	ft_isnotcharset(char c, char *set)
+void	ft_replace_char(char *str, char c, char rep)
 {
-	size_t	a;
+	int	a;
 
 	a = 0;
-	while (set[a])
+	if (!str || !c || !rep)
+		return ;
+	while (str[a])
 	{
-		if (c == set[a])
-			return (0);
+		if (str[a] == c)
+			str[a] = rep;
 		a++;
 	}
-	return (1);
+	return ;
 }
